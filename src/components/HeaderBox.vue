c327420d0de26f1fde44411a61ecf1be831f8e13
neshi00 <luca.ascione85@gmail.com>
2022-02-08 15:38:46 +0100

    layout jumbo done

--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -1,8 +1,21 @@
 <template>
-  <header class="container">
-    <div>logo</div>
+  <header class="skinny-container">
+    <div class="logo-box">
+      <img src="../assets/author-logo-round.png" alt="logo" />
+    </div>
 
-    <nav>navbar</nav>
+    <nav>
+      <ul>
+        <li><a href="#">testo</a></li>
+        <li>testo</li>
+        <li>testo</li>
+        <li>testo</li>
+        <li>testo</li>
+        <li>testo</li>
+        <li>testo</li>
+        <li>carrello</li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -10,4 +23,31 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+
+  color: #fff;
+
+  .logo-box {
+    height: 200px;
+
+    img {
+      height: 100%;
+    }
+  }
+
+  nav {
+    line-height: 100px;
+
+    ul {
+      display: flex;
+
+      li {
+        padding: 0 5px;
+      }
+    }
+  }
+}
+</style>
--- src/components/JumbotronTop.vue
+++ src/components/JumbotronTop.vue
@@ -1,5 +1,48 @@
 <template>
-  <header-box />
+  <div class="jumbo">
+    <header-box />
+
+    <div class="jumbo-container">
+      <div class="jumbo-text-box">
+        <h2>Damon Blabla</h2>
+        <div class="text">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ut, velit
+          dolorem? Vero iusto dolore mollitia, quae illum autem fuga natus
+          suscipit tenetur ipsa, eum corporis iste, quod qui quaerat sequi?
+        </div>
+        <p>The New York Times</p>
+        <div>
+          <button>freccia sx</button>
+          <button>freccia dx</button>
+        </div>
+      </div>
+
+      <div class="latest-release">
+        <div class="inner-box">
+          <div class="box-flex">
+            <div class="widget-microbox">
+              test
+              <img src="../assets/book-widget.png" alt="" />
+            </div>
+            <div class="widget-text">
+              <h3>testo</h3>
+              <p>AUTOBIOGRAPHY</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+                quos inventore porro, in, saepe ipsam, facilis pariatur hic
+                nostrum quasi illo neque blanditiis ullam odio accusamus.
+                Tempore provident qui recusandae?
+              </p>
+            </div>
+          </div>
+          <div class="button-box">
+            <button>sx</button>
+            <button>dx</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,4 +55,66 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.jumbo {
+  border: 5px solid blue;
+  height: 100vh;
+  background: url(../assets/hero-07-2x.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  header {
+    height: 30%;
+  }
+
+  .jumbo-container {
+    height: 70%;
+    display: flex;
+
+    .jumbo-text-box {
+      width: 60%;
+
+      border: 5px solid yellow;
+      color: #ffffff;
+    }
+
+    .latest-release {
+      position: relative;
+      width: 40%;
+      border: 5px solid green;
+
+      .inner-box {
+        background-color: #ffffff;
+        position: absolute;
+        bottom: 0;
+        border: 5px solid coral;
+
+        .box-flex {
+          display: flex;
+
+          .widget-microbox {
+            border: 3px solid cadetblue;
+            width: 50%;
+
+            img {
+              width: 100%;
+            }
+          }
+
+          .widget-text {
+            width: 50%;
+          }
+        }
+
+        .button-box {
+          display: flex;
+
+          button {
+            width: 50%;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
